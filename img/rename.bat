6de3503cc8b59b4aee8a5c649d60ceb13464f23b
KiranRiddle7 <kiranriddle.kc29@gmail.com>
2019-06-17 15:58:32 -0400

    added quotes images 365

---
+++ img/rename.bat
@@ -0,0 +1,30 @@
+xcopy S:\Misc\SriSaiTrust\img\quotes\img_1.jpg S:\Misc\SriSaiTrust\img
+ren	"img_1.jpg"	"img_141.jpg"
+
+xcopy S:\Misc\SriSaiTrust\img\quotes\img_2.jpg S:\Misc\SriSaiTrust\img
+ren	"img_2.jpg"	"img_142.jpg"
+
+xcopy S:\Misc\SriSaiTrust\img\quotes\img_3.jpg S:\Misc\SriSaiTrust\img
+ren	"img_3.jpg"	"img_143.jpg"
+
+xcopy S:\Misc\SriSaiTrust\img\quotes\img_4.jpg S:\Misc\SriSaiTrust\img
+ren	"img_4.jpg"	"img_144.jpg"
+
+xcopy S:\Misc\SriSaiTrust\img\quotes\img_5.jpg S:\Misc\SriSaiTrust\img
+ren	"img_5.jpg"	"img_145.jpg"
+
+xcopy S:\Misc\SriSaiTrust\img\quotes\img_6.jpg S:\Misc\SriSaiTrust\img
+ren	"img_6.jpg"	"img_146.jpg"
+
+xcopy S:\Misc\SriSaiTrust\img\quotes\img_7.jpg S:\Misc\SriSaiTrust\img
+ren	"img_7.jpg"	"img_147.jpg"
+
+xcopy S:\Misc\SriSaiTrust\img\quotes\img_8.jpg S:\Misc\SriSaiTrust\img
+ren	"img_8.jpg"	"img_148.jpg"
+
+xcopy S:\Misc\SriSaiTrust\img\quotes\img_9.jpg S:\Misc\SriSaiTrust\img
+ren	"img_9.jpg"	"img_149.jpg"
+
+xcopy S:\Misc\SriSaiTrust\img\quotes\img_10.jpg	S:\Misc\SriSaiTrust\img
+ren	"img_10.jpg" "img_150.jpg"
+
